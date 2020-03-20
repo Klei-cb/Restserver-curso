53cac46d6f6d385515df1feb689771423f782705
Kleiton Cambronero <1030klei@gmail.com>
2020-03-20 17:29:08 -0600

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -1,18 +1,18 @@
-//==============
+//========================
 // Puerto
-//==============
+//========================
 
 process.env.PORT = process.env.PORT || 3000;
 
-//==============
+//========================
 // Entorno
-//==============
+//========================
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-//==============
+//========================
 // Base de datos
-//==============
+//========================
 
 let urlDB;
 
@@ -23,3 +23,15 @@ if (process.env.NODE_ENV === 'dev') {
 }
 
 process.env.URLDB = urlDB;
+
+//========================
+// Vencimiento del token
+//========================
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+//========================
+// SEED de autenticación
+//========================
+
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
